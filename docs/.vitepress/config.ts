eda5579ce8dc20055837f4d8f61f4f8eb004133b
Imad Abdul-Karim <imad-ak@hotmail.com>
2023-05-21 12:30:56 +0200

    chore(docs): add favicon and sitemap

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,35 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Vue Better Transition',
   description: 'Use v-if on transition ✨',
+  head: [
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+    ],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
